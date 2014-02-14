429db0a26ce6f35304a5db89387463f7fde984d7
Craige McWhirter <craige@mcwhirter.com.au>
2014-02-14 23:31:49 +1100

    Yeah I know I said I was finished but know it's better

--- broken_links.sh
+++ broken_links.sh
@@ -4,8 +4,9 @@
 # file and outputs a file of links that still failed.
 # It's rought and ready with no error handling, enjoy.
 
-# Where is your curl?
+# Where are your tools?
 curl=/usr/bin/curl
+wc=/usr/bin/wc
 
 # Request the file to be read in
 echo "Enter the path and file name that contains the broken URLs: "
@@ -14,12 +15,12 @@ read urls
 echo "Enter the full or relative path & file name for the list of still broken URLs:"
 read broken
 
-# Initialise the array
+# Initialise the arrays
 index=0
 
 # Loop through the array
 while read url ; do
-	URLARRAY[$index]="$url"
+	# Let the use know what URL you're checking
 	echo "Now checking: $url"
 	# Set STATUS_CODE to use the curl command
 	STATUS_CODE=`$curl --output /dev/null --silent --head --write-out '%{http_code}\n' $url`
@@ -32,6 +33,13 @@ while read url ; do
 	index=$(($index+1))
 done < $urls
 
+# Count the broken URLs
+still_broken_URLs=`$wc -l $broken`
+
+echo
+# Print the number of URLs checked
 echo "Total URLs in the file: ${index}"
+# Print the number of broken URLs still present
+echo "Total URLs still broken: $still_broken_URLs"
 
 exit 0
