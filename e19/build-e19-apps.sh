4b786dca9b4f58ce495d0dd1b52622936e8309c9
Craige McWhirter <craige@mcwhirter.com.au>
2014-07-30 13:12:45 +1000

    Added e19 scripts

---
+++ e19/build-e19-apps.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Original version of this script based on sample sourced from Enlightenment
+# Contributing page: http://enlightenment.org/p.php?p=contribute&l=en
+
+##############
+# WARNING
+# This script is nasty hack with no error control.
+# This script assumes you have installed the build dependencies recommended.
+# It also installs and builds everything into /opt/e18
+# Best of luck!
+
+set -e
+# Where is the source code?
+SRCPATH="/home/craige/git/e18"
+# My quick and dirty dev install location
+PREFIX="/opt/e18"
+APPS=(
+	terminology
+	)
+SITE="git.enlightenment.org"
+GIT="git://$SITE/apps"
+OPT="--prefix=$PREFIX"
+
+export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PATH="$PREFIX/bin:$PATH"
+export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+
+for package in "${APPS[@]}"
+do
+	# Check the source is already there
+	if [ -a $SRCPATH/$package ]
+	then
+		cd $SRCPATH/$package
+		make clean distclean || true
+		# Update the repos
+		echo "Commencing $package rebase operations master!"
+		git pull --rebase
+	else
+		# No source? Clone it!
+		cd $SRCPATH
+		echo "Got to clone the $package source sir!"
+		git clone $GIT/$package.git
+		cd $SRCPATH/$package
+	fi
+	cd $SRCPATH/$package
+	./autogen.sh $OPT
+	make
+	sudo make install
+	cd ../..
+	sudo ldconfig
+done
---
+++ e19/build-e19.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Original version of this script based on sample sourced from Enlightenment
+# Contributing page: http://enlightenment.org/p.php?p=contribute&l=en
+
+##############
+# WARNING
+# This script is nasty hack with no error control.
+# This script assumes you have installed the build dependencies recommended.
+# It also installs and builds everything into /opt/e19
+# Best of luck!
+
+#set -eu
+# Where is the source code?
+SRCPATH="/home/craige/git/e19"
+# My quick and dirty dev install location
+PREFIX="/opt/e19"
+CORE=(
+	efl
+	elementary
+	evas_generic_loaders
+	emotion_generic_players
+	enlightenment
+	)
+SITE="git.enlightenment.org"
+GIT="git://$SITE/core"
+OPT="--prefix=$PREFIX"
+
+export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PATH="$PREFIX/bin:$PATH"
+export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
+
+for package in "${CORE[@]}"
+do
+	# Check the source is already there
+	if [ -a $SRCPATH/$package ]
+	then
+		cd $SRCPATH/$package
+		make clean distclean || true
+		# Update the repos
+		echo "Commencing $package rebase operations master!"
+		git pull --rebase
+	else
+		# No source? Clone it!
+		cd $SRCPATH
+		echo "Got to clone the $package source sir!"
+		git clone $GIT/$package.git
+		cd $SRCPATH/$package
+	fi
+	cd $SRCPATH/$package
+	./autogen.sh $OPT
+	make
+	sudo make install
+	cd ../..
+	sudo ldconfig
+done
