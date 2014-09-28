f00d74c7245edb8169933214f8608e9c6ea8c782
Craige McWhirter <craige@mcwhirter.com.au>
2014-09-28 22:33:30 +1000

    added debian-e19.sh

---
+++ e19/debian-e19.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Build e19 core for Debian
+# Contributing page: http://enlightenment.org/p.php?p=contribute&l=en
+
+##############
+# WARNING
+# This script is nasty hack with no error control.
+# Best of luck!
+
+set -eu
+
+# Step 1: Rename the upstream tarball
+# Step 2: Unpack the upstream tarball
+# Step 3: Add the Debian packaging files
+# Step 4: Build the package
+# Step 5: Install the package 
