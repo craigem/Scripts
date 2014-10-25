809fdc522dd9534bfce1ef72da2856e41142a508
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-25 11:25:38 +1100

    Created outline for gitweb_repo_build.sh

---
+++ git/gitweb_repo_build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# This script
+# * Builds a git repo hosted via gitweb
+# * Adds a git hook for automatically pushing to remotes
+# * Adds a remote for git hub
+
+set -eu
+
+### TO DO:
+# Where are your tools?
+#### LOCAL:
+# Create the local directory
+# Initialise the local repo
+# Add a README.mdwn and a LICENCE
+#### REMOTE
+# SSH to your server
+# Create the directory
+# Set the owner
+# Initialise the repo
+# Edit the description
+# Add a remote for github
+# Add a post-receive hook.
+#### LOCAL:
+# Add the new repo as a remote
+# Push it
+
+exit 0
