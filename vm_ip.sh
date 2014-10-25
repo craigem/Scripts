a8d36c6a62e54d0b8734d05d7c858e7df9b5ed9e
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-25 11:13:22 +1100

    Added modifications recommended by ShellCheck

--- vm_ip.sh
+++ vm_ip.sh
@@ -15,9 +15,9 @@ VM_NAME=$1
 export LIBVIRT_DEFAULT_URI=qemu:///system
 
 # Obtain the MAC address from libvirt:
-MAC_ADDRESS=`$virsh dumpxml $VM_NAME | $grep "mac address" | $sed "s/.*'\(.*\)'.*/\1/g"`
+MAC_ADDRESS=$($virsh dumpxml "$VM_NAME" | $grep "mac address" | $sed "s/.*'\(.*\)'.*/\1/g")
 
 # Use arp to find the IP address you're looking for via it's MAC address:
-$arp -an | $grep $MAC_ADDRESS
+$arp -an | $grep "$MAC_ADDRESS"
 
 exit 0
