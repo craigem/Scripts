bca3741fb92d3e884b103f687c36ab8d38f0960c
Craige McWhirter <craige@mcwhirter.com.au>
2014-11-24 16:48:59 +1100

    Set DP2 to the right.

--- lcd-T430-work.sh
+++ lcd-T430-work.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
 # Turn on the external monitor at work:
-/usr/bin/xrandr --output DP2 --primary ; /usr/bin/xrandr --output LVDS1 --mode 1600x400; /usr/bin/xrandr --output DP2 --mode 2560x1440; /usr/bin/xrandr --output DP2 --left-of LVDS1
+/usr/bin/xrandr --output DP2 --primary ; /usr/bin/xrandr --output LVDS1 --mode 1600x400; /usr/bin/xrandr --output DP2 --mode 2560x1440; /usr/bin/xrandr --output DP2 --right-of LVDS1
 echo "External LCD at work has been turned on"
 exit
