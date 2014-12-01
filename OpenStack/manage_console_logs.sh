a577b7780ceac863aae095f2cc8e300fd054f0c8
Craige McWhirter <craige@mcwhirter.com.au>
2014-12-02 10:35:36 +1100

    manage_console_logs now works

--- OpenStack/manage_console_logs.sh
+++ OpenStack/manage_console_logs.sh
@@ -10,12 +10,14 @@ TAIL=/usr/bin/tail
 # Where are your console logs?
 CONSOLE_LOG_PATH=/tmp/instances
 # Get a list of directories that end with a UUID in the console log path:
-LIST_INSTANCE_DIRS=$($FIND $CONSOLE_LOG_PATH/* -maxdepth 0 -type d | $GREP -E "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$")
+LIST_INSTANCE_DIRS=($($FIND $CONSOLE_LOG_PATH/* -maxdepth 0 -type d -printf "%f\n" | $GREP -E "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"))
 
 # for each instance, truncate the console log.
 for i in "${LIST_INSTANCE_DIRS[@]}"; do
     # Tail the last 1000 lines and overwrite the log file with the output
-    echo "`$TAIL -n 1000 $i/console.log`" > $i/console.log
+    echo "$($TAIL -n 1000 $CONSOLE_LOG_PATH/$i/console.log)" > "$CONSOLE_LOG_PATH/$i/console.log"
+    # echo "$CONSOLE_LOG_PATH/$i"
+    # echo "Hello"
 done
 
 exit 0
