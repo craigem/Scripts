2631886a9743cd302b2a92dc4741abaa61dcd090
Craige McWhirter <craige@mcwhirter.com.au>
2015-01-01 23:06:12 +1100

    got it finding the first directory

--- timemachine_photos.py
+++ timemachine_photos.py
@@ -1,14 +1,16 @@
 """You have an external drive with Apple's TimeMachine backups on it. You would
-like to recover all the photos from one particular backup."""
+like to recover all the photos from one particular backup.
 
-# This script
-# * Takes the following arguments:
-#   * $1 is the Drive location ie: usb0 or usb1
-#   * $2 is the username
-#   * $3 is the date you wish to recover from ie: 2014-04-19-131501
-#   * $4 is where you wish the output to go
-# * It will locate all the photos backed up on that date
-# * Then copy them to your preferred location
+This script
+* Takes the following arguments:
+  * $1 is the Drive location ie: usb0 or usb1
+  * $2 is the username
+  * $3 is the date you wish to recover from ie: 2014-04-19-131501
+  * $4 is where you wish the output to go
+* It will locate all the photos backed up on that date
+* Then copy them to your preferred location"""
+
+import os
 
 # Environment
 # Where are your tools?
@@ -19,7 +21,8 @@ like to recover all the photos from one particular backup."""
 
 # What has the user given us?
 # readonly DRIVE="/media/$1/Time Machine Backups"
-# readonly DRIVE="/media/$1"
+# DRIVE = "/media/$1"
+DRIVE = "/media/usb1"
 # readonly USERNAME=$2
 # readonly RECOVERY_DATE=$3
 # readonly COPY_TO=$4
@@ -29,6 +32,7 @@ like to recover all the photos from one particular backup."""
 
 def start():
     """Run the bulk of the script"""
+    print "Start finding the pictures"
     find_pictures()
     copy_pictures()
     check_pictures()
@@ -36,12 +40,19 @@ def start():
 
 def find_pictures():
     """Locate Pictures folder and get it's number"""
-    print "find the pictures"
+    print "Find the pictures"
     # Make the first character of the user name upper-case to conform to the
     # default Mac names
     # local username=${USERNAME[@]^}
     # Find the path for the recovery date
     # local date_path="$($SUDO $FIND "$DRIVE" -name $RECOVERY_DATE)"
+    """for media in os.listdir(DRIVE):
+        if media.endswith(".jpg"):
+            print media"""
+    for root, dirs, files in os.walk("/media/usb1"):
+        for media in dirs:
+            if media.endswith("_8374224"):
+                print os.path.join(root, media)
     # local pictures_path="$($SUDO $FIND "$date_path" -name Pictures)"
     # local pictures_number="$($LS -lah "$pictures_path" | $AWK '{print $2}')"
     # Use it's number from the second column to locate the real Pictures data
