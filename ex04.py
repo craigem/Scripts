e101806fae9425f97b458f0bcbe32ddaa4209d75
Craige McWhirter <craige@mcwhirter.com.au>
2015-01-04 21:51:21 +1100

    Completed ex04

---
+++ ex04.py
@@ -0,0 +1,11 @@
+"""Introduction to Python - Exercise 04
+
+First Name Cases
+* Store your first name, in lowercase, in a variable.
+* Using that one variable, print your name in lowercase, Titlecase, and
+UPPERCASE."""
+
+FIRST_NAME = "craige"
+print FIRST_NAME.lower()
+print FIRST_NAME.title()
+print FIRST_NAME.upper()
