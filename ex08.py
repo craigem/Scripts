8566e344a918561134e2eaa23e29c6be3df69853
Craige McWhirter <craige@mcwhirter.com.au>
2015-01-05 09:58:36 +1100

    Completed ex08.

---
+++ ex08.py
@@ -0,0 +1,12 @@
+"""Introduction to Python - Exercise 8
+
+Arithmetic
+* Write a program that prints out the results of at least one calculation for
+each of the basic operations: addition, subtraction, multiplication, division,
+and exponents."""
+
+print 6 + 180
+print 180 - 6
+print 6 * 180
+print 180 / 6
+print 6 ** 180
