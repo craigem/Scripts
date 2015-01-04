0732de2d3ea6a651f96578c2ba03f57365fae39d
Craige McWhirter <craige@mcwhirter.com.au>
2015-01-05 10:03:35 +1100

    Completed ex09.

---
+++ ex09.py
@@ -0,0 +1,13 @@
+"""Introduction to Python - Exercise 9
+
+Order of Operations
+* Find a calculation whose result depends on the order of operations.
+* Print the result of this calculation using the standard order of operations.
+* Use parentheses to force a non-standard order of operations. Print the result
+of this calculation."""
+
+STANDARD_ORDER = 6 + 180 / 5
+print STANDARD_ORDER
+
+MY_ORDER = (6 + 180) / 5
+print MY_ORDER
