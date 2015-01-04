7529c747865d381907012d4917883c05e920eda3
Craige McWhirter <craige@mcwhirter.com.au>
2015-01-05 10:22:58 +1100

    Completed ex10.

---
+++ ex10.py
@@ -0,0 +1,12 @@
+"""Introduction to Python - Exercise 10
+
+Long Decimals
+* On paper, 0.1+0.2=0.3. But you have seen that in Python,
+0.1+0.2=0.30000000000000004.
+* Find at least one other calculation that results in a long decimal like this.
+"""
+
+print 3 * 0.1
+
+# Python returned 0.3 for me but it was supposed to return 0.30000000000000004
+# I need to work out why.
