93df813f64169f551726d5f94e6f3afb0074cd32
Craige McWhirter <craige@mcwhirter.com.au>
2015-01-04 21:54:31 +1100

    Completed ex05

---
+++ ex05.py
@@ -0,0 +1,9 @@
+"""Introduction to Python - Exercise 5
+
+Full Name
+* Store your first name and last name in separate variables, and then combine
+them to print out your full name."""
+
+FIRST_NAME = "craige"
+LAST_NAME = "mcwhirter"
+print FIRST_NAME + " " + LAST_NAME
