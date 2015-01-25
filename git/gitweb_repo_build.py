99a8b9cbc69ae8202cfcb51d7ea239f45037b8fa
Craige McWhirter <craige@mcwhirter.com.au>
2015-01-26 00:45:27 +1100

    Began re-writing gitweb_repo_build as a python programme

---
+++ git/gitweb_repo_build.py
@@ -0,0 +1,50 @@
+'''
+USE CASE:
+You have a local git repo that you wish to push to both your own git server,
+github and bitbucket.
+
+This script
+* Reads your local config file
+* Gets repo name and description from arguments
+* Builds git repo locally
+* Adds a README.md and a LICENCE. Commits the changes.
+* Builds a git repo hosted via remote git server
+* Adds a git hook for automatically pushing to configured remotes.
+* Adds a remote for github.
+* Adds a remote for bitbucket.
+* Creates a repo at GitHub.
+* Creates a repo at bitbucket.
+* Pushes to remote.
+* Checks remote and github are working correctly.
+'''
+
+import ConfigParser
+import os
+
+# Get HOME
+HOME = os.getenv('HOME')
+RCFILE = HOME + "/.gitweb_repo_build.rc"
+# Read the variables from the local config file
+CONFIG = ConfigParser.ConfigParser()
+CONFIG.read(RCFILE)
+GITDIR = CONFIG.get("grb", "GITDIR")
+GITHUBUSER = CONFIG.get("grb", "GITHUBUSER")
+BITBUCKETUSER = CONFIG.get("grb", "BITBUCKETUSER")
+GITSERVER = CONFIG.get("grb", "GITSERVER")
+GITREMOTEDIR = CONFIG.get("grb", "GITREMOTEDIR")
+LICENSE = CONFIG.get("grb", "LICENSE")
+
+# Get repo name and description for the CLI
+# REPO=$1
+# DESCRIPTION=$2
+
+# Builds git repo locally
+# Adds a README.md and a LICENCE. Commits the changes.
+# Builds a git repo hosted via remote git server
+# Adds a git hook for automatically pushing to configured remotes.
+# Adds a remote for github.
+# Adds a remote for bitbucket.
+# Creates a repo at GitHub.
+# Creates a repo at bitbucket.
+# Pushes to remote.
+# Checks remote and github are working correctly.
